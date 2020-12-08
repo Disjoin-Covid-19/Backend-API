bf69bd6203a010228e7970ded44a5362d9f03660
singhguri <singhguri5598@gmail.com>
2020-12-08 20:28:38 +0530

    Store Id update Fix

--- app/requestMethods.py
+++ app/requestMethods.py
@@ -1,13 +1,15 @@
-from config import client
 from app import app
 from bson.json_util import dumps
 from flask import request, jsonify
 import json, jwt, ast
-import pymongo
-
+from pymongo import MongoClient
 from geopy import distance
+import uuid
 
-db = client["DisJoin_data"]
+conn_client = MongoClient(
+    "mongodb+srv://DisJoin:1234@cluster0-bk4u2.mongodb.net/test?retryWrites=true&w=majority"
+)
+db = conn_client["DisJoin_data"]
 store_list_collection = db["storeList"]
 
 
@@ -28,36 +30,39 @@ def create_store_record():
     store_list = get_all_stores()
     data = request.get_json()
     try:
-        data["sid"] = len(store_list) + 1
+        # data["sid"] = len(store_list) + 1  # needs improvisation
+        data["sid"] = uuid.uuid4()
         record_created = store_list_collection.insert(data)
         return "", 201
     except Exception as e:
         return e, 500
 
 
+def within_range(store_list, center_point, radius):
+    response_list = []
+    for store in store_list:
+        points = store["coordinates"]
+        store_point_tuple = tuple(points)
+        center_point_tuple = tuple(center_point)
+
+        dis = distance.distance(center_point_tuple, store_point_tuple).km
+
+        if dis <= radius:
+            response_list.append(store)
+
+
 @app.route("/api/v1/geofence_stores", methods=["GET"])
 def get_stores_by_geofence():
     try:
         data = request.get_json()
-    except Exception as e:
-        return e, 300
-    try:
-        query = store_list_collection.find()
-        store_list = [store for store in query]
 
-        response_list = []
-        center = data["center"]
+        store_collection = store_list_collection.find()
+        store_list = [store for store in store_collection]
+
+        center_point = data["center"]
         radius = data["radius"]
 
-        for store in store_list:
-            points = store["coordinates"]
-            test_tuple = tuple(points)
-            center_tuple = tuple(center)
-
-            dis = distance.distance(center_tuple, test_tuple).km
-
-            if dis <= radius:
-                response_list.append(store)
+        response_list = within_range(store_list, center_point, radius)
 
         return dumps(response_list), 200
 
@@ -72,6 +77,6 @@ def page_not_found(e):
             "msg": "This route is currently not supported. Please refer API documentation."
         }
     }
-    resp = jsonify(user_message)
-    resp.status_code = 404
-    return resp
+    page_not_found_response = jsonify(user_message)
+    page_not_found_response.status_code = 404
+    return page_not_found_response
--- config.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient(
-    "mongodb+srv://DisJoin:1234@cluster0-bk4u2.mongodb.net/test?retryWrites=true&w=majority"
-)
